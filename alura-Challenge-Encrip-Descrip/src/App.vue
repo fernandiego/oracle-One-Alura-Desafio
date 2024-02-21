3ac7e81caee9ab76cc29b1ca15117f441ec0016e
fox <fernandodiego.sn@gmail.com>
2024-02-21 15:37:55 -0300

    Esse commit adiciona os botões, campo de texto input e output e possui o código para encriptar e descriptar um texto

---
+++ alura-Challenge-Encrip-Descrip/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="input-container">
+      <label for="text">Text to Encrypt/Decrypt:</label>
+      <input type="text" v-model="inputText" placeholder="Digite seu texto" id="text">
+      <div class="button-container">
+        <button @click="encrypt">Encrypt</button>
+        <button @click="decrypt">Decrypt</button>
+      </div>
+    </div>
+    <div class="output-container">
+      <textarea  v-model="outputText" readonly></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+import CryptoJS from 'crypto-js'; // Import CryptoJS library
+
+export default {
+  data() {
+    return {
+      inputText: "",
+      outputText: ""
+    };
+  },
+  methods: {
+    encrypt() {
+      this.outputText = CryptoJS.AES.encrypt(this.inputText, "secret_key").toString();
+    },
+    decrypt() {
+      try {
+        this.outputText = CryptoJS.AES.decrypt(this.inputText, "secret_key").toString(CryptoJS.enc.Utf8);
+      } catch (error) {
+        console.error("Decryption error:", error);
+        this.outputText = "Error: Unable to decrypt. Make sure the input is a valid encrypted text.";
+      }
+    }
+  }
+};
+</script>
---
+++ alura-Challenge-Encrip-Descrip/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import 'crypto-js'; // Import CryptoJS library
+
+createApp(App).mount('#app');
