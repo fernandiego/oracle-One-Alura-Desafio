ca19cd24d802f469dee22bdeef1569b6b46164d1
fox <fernandodiego.sn@gmail.com>
2024-02-23 20:15:36 -0300

    Trabalhando o CSS texto de aviso

--- alura-Challenge-Encrip-Descrip/src/App.vue
+++ alura-Challenge-Encrip-Descrip/src/App.vue
@@ -2,13 +2,22 @@
   <div class="container">
     <div class="input-container">
       <input class="input-text" type="text" v-model="inputText" placeholder="Digite seu texto" id="text">
+      <div class="texto-exclamcao">
+        <img src="./assets/bi_exclamation-circle-fill.svg">
+        Apenas letras minúsculas e sem acento.</div>
       <div class="button-container">
         <button class="button-cript" @click="encrypt">Encrypt</button>
         <button class="button-descript" @click="decrypt">Decrypt</button>
       </div>
     </div>
     <div class="output-container">
-      <textarea  v-model="outputText" readonly></textarea>
+      <textarea v-model="outputText" v-if="outputText"></textarea>
+      <img class="img" v-if="!outputText" src="./assets/High%20quality%20products%201%201.svg">
+      <div class="texto-nenhuma-mensagem">
+        Nenhuma mensagem encontrada
+      </div>
+      <div class="digite-um-texto">Digite um texto que você deseja criptografar ou descriptografar.</div>
+
     </div>
   </div>
 </template>
